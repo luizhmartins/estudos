add718e725a56b46004856331aaa1628c10feaf4
Luiz Martins <luiiz.martiins@outlook.com>
2022-07-01 19:45:20 -0300

    atualizei o exercicio de javascript

--- javascript-para-frontend/aluramidi/main.js
+++ javascript-para-frontend/aluramidi/main.js
@@ -1,1 +1,15 @@
-document.querySelector('.tecla_tom');
+function tocaSom (idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let c=0; c < listaDeTeclas.length; c++){
+  const tecla = listaDeTeclas[c];
+  const instrumento = tecla.classList[1];
+
+  tecla.onclick = function() {
+    tocaSom(`#som_${instrumento}`)
+  }
+}
